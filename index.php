8d9582d94848d814abe2c4d9e7c6370ca99e6b1a
ksg <ksg@toxic.network>
2023-08-07 17:55:05 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>GFS Presidents Club</title>
+  <meta content="GFS Presidents Club" name="keywords">
+  <meta content="GFS Presidents Club" name="description">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
+  <link href="assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="assets/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="assets/lib/hover/hover.min.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/css/responsive.css" rel="stylesheet">
+  <link rel="shortcut icon" href="images/favicon.png">
+</head>
+<body>
+  <div id="header" class="home">
+    <div class="container">	
+      <div class="header-content">
+        <h1>Welcome <span class="typed"></span></h1>
+        <p>GFS Presidents Club</p>
+        <ul class="list-unstyled list-social">
+		  <li><a href="https://github.com/null" target="_blank"><i class="ion-social-github"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/null/" target="_blank"><i class="ion-social-linkedin" target="_blank"></i></a></li>
+          <li><a href="https://twitter.com/@null" target="_blank"><i class="ion-social-twitter"></i></a></li>
+          <li><a href="https://www.youtube.com/channel/null"><i class="ion-social-youtube" target="_blank"></i></a></li>
+		  <li><a href="https://www.instagram.com/null/" target="_blank"><i class="ion-social-instagram"></i></a></li>
+		  <li><a href="https://www.facebook.com/null" target="_blank"><i class="ion-social-facebook"></i></a></li>
+      </div>
+    </div>
+  </div>
+  <script src="assets/lib/jquery/jquery.min.js"></script>
+  <script src="assets/lib/jquery/jquery-migrate.min.js"></script>
+  <script src="assets/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/lib/typed/typed.js"></script>
+  <script src="assets/lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="assets/lib/magnific-popup/magnific-popup.min.js"></script>
+  <script src="assets/lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
